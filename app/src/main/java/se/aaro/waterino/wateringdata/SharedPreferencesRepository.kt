a0f65b7badfb6084961f6a6d878659694ed45c41
Gustav Aaro <gustav.aaro@gmail.com>
2022-04-11 20:12:25 +0200

    Enable push notifications

--- app/src/main/java/se/aaro/waterino/wateringdata/SharedPreferencesRepository.kt
+++ app/src/main/java/se/aaro/waterino/wateringdata/SharedPreferencesRepository.kt
@@ -1,6 +1,7 @@
 package se.aaro.waterino.wateringdata
 
 import android.content.SharedPreferences
+import com.google.firebase.messaging.FirebaseMessaging
 import javax.inject.Inject
 
 class SharedPreferencesRepository @Inject constructor(
@@ -8,9 +9,14 @@ class SharedPreferencesRepository @Inject constructor(
 ) {
     companion object {
         private const val PUSH_NOTIFICATIONS_ENABLED = "ENABLE_NOTIFICATION"
+        private const val PLANT_DATA_FIREBASE_TOPIC = "plantData"
     }
 
     var pushNotificationsEnabled: Boolean
         get() = preferences.getBoolean(PUSH_NOTIFICATIONS_ENABLED, true)
-        set(enabled) = preferences.edit().putBoolean(PUSH_NOTIFICATIONS_ENABLED, enabled).apply()
+        set(enabled) {
+            preferences.edit().putBoolean(PUSH_NOTIFICATIONS_ENABLED, enabled).apply()
+            if (enabled) FirebaseMessaging.getInstance().subscribeToTopic(PLANT_DATA_FIREBASE_TOPIC)
+            else FirebaseMessaging.getInstance().unsubscribeFromTopic(PLANT_DATA_FIREBASE_TOPIC)
+        }
 }
